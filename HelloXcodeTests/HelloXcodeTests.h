941d7b6798a4e25dd27dee914ca03c1b14ff272d
Michael Colenso <michaelcolenso@mike-laptop.local>
2011-07-16 23:01:23 -0700

    Initial Commit

---
+++ HelloXcodeTests/HelloXcodeTests.h
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeTests.h
+//  HelloXcodeTests
+//
+//  Created by Michael Colenso on 7/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HelloXcodeTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ HelloXcodeTests/HelloXcodeTests.m
@@ -0,0 +1,33 @@
+//
+//  HelloXcodeTests.m
+//  HelloXcodeTests
+//
+//  Created by Michael Colenso on 7/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloXcodeTests.h"
+
+
+@implementation HelloXcodeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloXcodeTests");
+}
+
+@end
